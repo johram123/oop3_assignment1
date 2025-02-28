971e02c3a1f285374ff8570cad18cae0238bceba
Lloyd-Saclot <164666621+lloyd-saclot@users.noreply.github.com>
2025-02-28 08:28:31 -0700

    Update SortAlgorithm.java
    Added BubbleSort and SelectionSort (both base and height)

--- src/utilities/SortAlgorithm.java
+++ src/utilities/SortAlgorithm.java
@@ -5,13 +5,76 @@ import shapes.BaseAreaCompare;
 import shapes.VolumeCompare;
 import shapes.Shape;
 
-public class SortAlgorithm 
-{
-	// WILL HAVE TOTAL 12 SORTING ALGORITHM METHODS 2 for each type 1 Comparable for Comparable and 2nd for Comparator
-    public static <T extends Comparable<T>> void bubbleSort(T[] arr) 
-    {
-        
+public class SortAlgorithm {
+
+    // Bubble Sort using Comparable (height)
+    public static <T extends Comparable<T>> void bubbleSort(T[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j].compareTo(arr[j + 1]) < 0) {
+                    T temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
     }
+
+    // Bubble Sort using Comparator (base area/volume)
+    public static <T> void bubbleSort(T[] arr, Comparator<T> comparator) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (comparator.compare(arr[j], arr[j + 1]) < 0) {
+                    T temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    // Selection Sort using comparable (height)
+    public static <T extends Comparable<T>> void selectionSort(T[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int maxIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j].compareTo(arr[maxIndex]) > 0) {
+                    maxIndex = j;
+                }
+            }
+            T temp = arr[maxIndex];
+            arr[maxIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Selection Sort using comparator (base area/volume)
+    public static <T> void selectionSort(T[] arr, Comparator<T> comparator) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int maxIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (comparator.compare(arr[j], arr[maxIndex]) > 0) {
+                    maxIndex = j;
+                }
+            }
+            T temp = arr[maxIndex];
+            arr[maxIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
     
     public static void quickSort(Shape[] arr, int lowIndex, int highIndex) 
     {
