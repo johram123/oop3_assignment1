a476043da8634a37168f84fce37284c15c1e677e
johram123 <johramq@gmail.com>
2025-02-28 21:15:50 -0700

    Added loadShapes

--- src/manager/SortManager.java
+++ src/manager/SortManager.java
@@ -1,14 +1,18 @@
 package manager;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import shapes.BaseAreaCompare;
-import shapes.Shape;
+import shapes.*;
 import shapes.VolumeCompare;
 import utilities.SortAlgorithm;
 
 
 public class SortManager {
 	
-	private Shape[] shapes;
+	public Shape[] shapes;
     private String fileName;
     private char compareType;
     private char sortType;
@@ -86,11 +90,63 @@ public class SortManager {
 	
 	/**
 	 * Loads the shapes into the shapes array
+	 * @throws FileNotFoundExceprion
 	 */
 	
 	private void loadShapes() 
-	{
-		// TODO Auto-generated method stub
+	{		
+		try 
+		{
+			File file = new File(fileName);
+			Scanner readFile = new Scanner(file);
+			int numOfShapes = Integer.parseInt(readFile.nextLine());
+			shapes = new Shape[numOfShapes];
+			int counter = 0; 
+			
+			while (readFile.hasNext())
+			{
+				String line = readFile.nextLine();
+				String[] field = line.split(" ");
+				String shape = field[0];
+				
+				switch (shape)
+				{
+					case "Cone":
+						shapes[counter] = new Cone(Double.parseDouble(field[1]), Double.parseDouble(field[2]));
+						counter++;
+						break;
+					case "Cylinder":
+						shapes[counter] = new Cylinder(Double.parseDouble(field[1]), Double.parseDouble(field[2]));
+						counter++;
+						break;
+					case "Pyramid":
+						shapes[counter] = new Pyramid(Double.parseDouble(field[1]), Double.parseDouble(field[2]));
+						counter++;
+						break;
+					case "SquarePrism":
+						shapes[counter] = new SquarePrism(Double.parseDouble(field[1]), Double.parseDouble(field[2]));
+						counter++;
+						break;
+					case "TriangularPrism":
+						shapes[counter] = new TriangularPrism(Double.parseDouble(field[1]), Double.parseDouble(field[2]));
+						counter++;
+						break;
+					case "PentagonalPrism":
+		        		shapes[counter] = new PentagonalPrism(Double.parseDouble(field[1]), Double.parseDouble(field[2]));
+		        		counter++;
+		        		break;
+					case "OctagonalPrism":
+						shapes[counter] = new OctagonalPrism(Double.parseDouble(field[1]), Double.parseDouble(field[2]));
+						counter++;
+						break;
+				}	
+			}
+			readFile.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println("File not found!");
+		}
 		
 	}
 }
