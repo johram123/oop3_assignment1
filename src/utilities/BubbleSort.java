022b7e6c19c1324e2fcbf19d0f24ba6e72d23f69
shawn  zabala francia <159972209+ShawnFrancia@users.noreply.github.com>
2025-02-28 23:44:36 -0700

    Update BubbleSort.java
    added java doc

--- src/utilities/BubbleSort.java
+++ src/utilities/BubbleSort.java
@@ -2,6 +2,13 @@ package utilities;
 
 import java.util.Comparator;
 
+/**
+ * Implements the Bubble Sort algorithm.
+ * @param <T> type of elements to be sorted
+ * @param arr array to be sorted
+ * @author Shawn, Johram, Lloyd
+ */
+
 public class BubbleSort {
 	// Bubble Sort using Comparable (height)
     public static <T extends Comparable<T>> void bubbleSort(T[] arr) {
@@ -21,6 +28,13 @@ public class BubbleSort {
         }
     }
 
+     /**
+     * Sorts an array using Bubble Sort with comparator.
+     * @param <T> elements to be sorted
+     * @param arr array to be sorted
+     * @param comparator comparator to compare elements
+     */
+	
     // Bubble Sort using Comparator (base area/volume)
     public static <T> void bubbleSort(T[] arr, Comparator<T> comparator) {
         int n = arr.length;
